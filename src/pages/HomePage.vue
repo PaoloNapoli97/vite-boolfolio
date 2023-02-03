80d70f3ca794b8aa961deb88a8316ad42cd70cb7
PaoloNapoli97 <paolonapoli@live.it>
2023-02-03 02:15:41 +0100

    router

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>Homepage</h2>
+    <ListProjects />
+  </div>
+</template>
+
+<script>
+import ListProjects from "../components/ListProjects.vue";
+
+export default {
+  name: "HomePage",
+  components: {
+    ListProjects,
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
